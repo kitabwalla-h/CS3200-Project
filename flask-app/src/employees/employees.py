5b469adbd544ae1887f901aa069dc1dcf18eced2
ebalo03 <148168183+ebalo03@users.noreply.github.com>
2023-12-03 14:13:55 -0500

    Updated employees.py

--- flask-app/src/employees/employees.py
+++ flask-app/src/employees/employees.py
@@ -5,13 +5,13 @@ from src import db
 
 employees = Blueprint('employees', __name__)
 
-# Get all the products from the database
+# Get all employee info from the database
 @employees.route('/employees', methods=['GET'])
 def get_employees():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
-    # use cursor to query the database for a list of products
+    # use cursor to query the database for a list of employees
     cursor.execute('SELECT * FROM HuskyEatzEmployee')
 
     # grab the column headers from the returned data
@@ -31,80 +31,7 @@ def get_employees():
 
     return jsonify(json_data)
 
-@employees.route('/employees/<id>', methods=['GET'])
-def get_product_detail (id):
-
-    query = 'SELECT id, product_name, description, list_price, category FROM products WHERE id = ' + str(id)
-    current_app.logger.info(query)
-
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    column_headers = [x[0] for x in cursor.description]
-    json_data = []
-    the_data = cursor.fetchall()
-    for row in the_data:
-        json_data.append(dict(zip(column_headers, row)))
-    return jsonify(json_data)
-    
-
-# get the top 5 products from the database
-@employees.route('/mostExpensive')
-def get_most_pop_products():
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT product_code, product_name, list_price, reorder_level
-        FROM products
-        ORDER BY list_price DESC
-        LIMIT 5
-    '''
-    cursor.execute(query)
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
-
-
-@employees.route('/tenMostExpensive', methods=['GET'])
-def get_10_most_expensive_products():
-    
-    query = '''
-        SELECT product_code, product_name, list_price, reorder_level
-        FROM products
-        ORDER BY list_price DESC
-        LIMIT 10
-    '''
-
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-    
-    return jsonify(json_data)
-
+# add new employees to database
 @employees.route('/employees', methods=['POST'])
 def add_new_employee():
     
@@ -112,18 +39,18 @@ def add_new_employee():
     the_data = request.json
     current_app.logger.info(the_data)
 
-    #extracting the variable
-    name = the_data['product_name']
-    description = the_data['product_description']
-    price = the_data['product_price']
-    category = the_data['product_category']
+    #extracting the variables
+    first_name = the_data['FirstName']
+    last_name = the_data['LastName']
+    role = the_data['Role']
+    employee_id = the_data['EmployeeID']
 
     # Constructing the query
-    query = 'insert into HuskyEatzEmployee (FirstName, LastName, Role) values ("'
-    query += name + '", "'
-    query += description + '", "'
-    query += category + '", '
-    query += str(price) + ')'
+    query = 'INSERT INTO HuskyEatzEmployee (FirstName, LastName, Role, EmployeeID) VALUES ("'
+    query += first_name + '", "'
+    query += last_name + '", "'
+    query += role + '", '
+    query += str(employee_id) + ')'
     current_app.logger.info(query)
 
     # executing and committing the insert statement 
@@ -133,25 +60,39 @@ def add_new_employee():
     
     return 'Success!'
 
-### Get all product categories
-@employees.route('/categories', methods = ['GET'])
-def get_all_categories():
-    query = '''
-        SELECT DISTINCT category AS label, category as value
-        FROM products
-        WHERE category IS NOT NULL
-        ORDER BY category
-    '''
-
+# update employee details
+@employees.route('/employees/<EmployeeID>', methods=['PUT'])
+def update_employee(employee_id):
     cursor = db.get_db().cursor()
-    cursor.execute(query)
 
-    json_data = []
-    # fetch all the column headers and then all the data from the cursor
-    column_headers = [x[0] for x in cursor.description]
-    theData = cursor.fetchall()
-    # zip headers and data together into dictionary and then append to json data dict.
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variables
+    first_name = the_data['FirstName']
+    last_name = the_data['LastName']
+    role = the_data['Role']
+
+    # constructing the query
+    query = 'UPDATE HuskyEatzEmployee SET FirstName = %s, LastName = %s, Role = %s WHERE EmployeeID = %s'
+
+    cursor.execute(query, (first_name, last_name, role, employee_id))
+    db.get_db().commit()
+
+    return 'Success!'
     
-    return jsonify(json_data)
+# Delete employee from the database
+@employees.route('/employees/<EmployeeID>', methods=['DELETE'])
+def delete_employee(employee_id):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to delete an employee
+    query = 'DELETE FROM HuskyEatzEmployee WHERE EmployeeID = %s'
+    cursor.execute(query, (employee_id))
+
+    # commit changes to the database
+    db.get_db().commit()
+
+    return 'Success!'
