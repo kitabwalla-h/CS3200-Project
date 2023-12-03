beeb08041264c3ce9ed4cd14fd97d3c6f043c640
michelle-vel <michellevsep@gmail.com>
2023-12-03 16:30:24 -0500

    testing orders file

--- flask-app/src/orders/orders.py
+++ flask-app/src/orders/orders.py
@@ -7,15 +7,16 @@ orders = Blueprint('orders', __name__)
 
 # Get all the orders given a specific vendor 
 @orders.route('/orders/<vendor_id>', methods=['GET'])
-def get_orders_by_vendor():
+def get_orders_by_vendor(vendor_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
     # use cursor to query the database for a list of products
-    cursor.execute('SELECT * FROM Order WHERE VendorID = vendor_id')
+    query = 'SELECT * FROM Order WHERE VendorID = ' + str(vendor_id)
+    cursor.execute(query)
 
     # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
+    row_headers = [x[0] for x in cursor.description]
 
     # create an empty dictionary object to use in 
     # putting column headers together with data
@@ -27,18 +28,20 @@ def get_orders_by_vendor():
     # for each of the rows, zip the data elements together with
     # the column headers. 
     for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 # Get the status of a specific order
-@orders.route('/orders/<order_details_id>/<status>', methods=['GET'])
-def get_order_status():
+@orders.route('/orders/<order_details_id>/status', methods=['GET'])
+def get_order_status(order_details_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
-
+    query = 'SELECT * FROM OrderStatus WHERE OrderDetailsID =' + str(order_details_id)
     # use cursor to query the database for a list of products
-    cursor.execute('SELECT * FROM OrderStatus WHERE OrderDetailsID = order_details_id')
+    cursor.execute(query)
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
