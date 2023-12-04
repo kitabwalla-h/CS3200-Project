2879218b426a5b4616b8e04f5bc2cd73e5f4b897
kitabwalla-h <76460205+kitabwalla-h@users.noreply.github.com>
2023-12-04 16:06:24 -0500

    Update vendors.py

--- flask-app/src/vendors/vendors.py
+++ flask-app/src/vendors/vendors.py
@@ -7,7 +7,7 @@ vendors = Blueprint('vendors', __name__)
 
 # Get all the vendors from the database
 @vendors.route('/vendors', methods=['GET'])
-def get_products():
+def get_vendors():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
@@ -33,7 +33,7 @@ def get_products():
 
 # get specific vendor details
 @vendors.route('/vendors/<vendor_id>', methods=['GET'])
-def get_vendor_detail (vendor_id):
+def get_vendor (vendor_id):
 
     query = 'SELECT * FROM Vendor WHERE VendorID = ' + str(vendor_id)
     current_app.logger.info(query)
@@ -47,243 +47,131 @@ def get_vendor_detail (vendor_id):
         json_data.append(dict(zip(column_headers, row)))
     return jsonify(json_data)
 
+# get vendors under a specific vendor type
+@vendors.route('/vendors/vendor_type/<vendor_type>', methods=['GET'])
+def get_vendor_type (vendor_type):
 
+    query = 'SELECT * FROM Vendor WHERE VendorType = "' + str(vendor_type) + '";'
+    current_app.logger.info(query)
 
-# # get vendors under a specific vendor type
-# @vendors.route('/vendors/<vendor_type>', methods=['GET'])
-# def get_vendor_detail (vendor_type):
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
 
-#     query = 'SELECT * FROM Vendor WHERE VendorType = ' + str(vendor_type)
-#     current_app.logger.info(query)
+# get vendors under a specific cuisine type
+@vendors.route('/vendors/cusine_type/<cuisine_type>', methods=['GET'])
+def get_vendor_detail (cuisine_type):
 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     column_headers = [x[0] for x in cursor.description]
-#     json_data = []
-#     the_data = cursor.fetchall()
-#     for row in the_data:
-#         json_data.append(dict(zip(column_headers, row)))
-#     return jsonify(json_data)
-
-# # get vendors under a specific cuisine type
-# @vendors.route('/vendors/<cuisine_type>', methods=['GET'])
-# def get_vendor_detail (cuisine_type):
-
-#     query = 'SELECT * FROM Vendor WHERE CuisineType = ' + str(cuisine_type)
-#     current_app.logger.info(query)
+    query = 'SELECT * FROM Vendor WHERE CuisineType = "' + str(cuisine_type) + '";'
+    current_app.logger.info(query)
     
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     column_headers = [x[0] for x in cursor.description]
-#     json_data = []
-#     the_data = cursor.fetchall()
-#     for row in the_data:
-#         json_data.append(dict(zip(column_headers, row)))
-#     return jsonify(json_data)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
     
 
-# # get the prices of menu items from a specific menu
-# @vendors.route('/vendors/<vendor_id>/<menu_id>/prices')
-# def get_menu_prices(vendor_id, menu_id):
-#     cursor = db.get_db().cursor()
-#     query = '''
-#         SELECT mi.Name, mi.Price
-#         FROM Vendor v
-#         JOIN Menu m on v.VendorID = m.VendorID
-#         JOIN MenuItems mi on mi.MenuID = m.MenuID
-#         WHERE v.VendorID = ' + str(vendor_id) and m.MenuID = ' + str(menu_id)
-#         ORDER BY mi.Price DESC
-#     '''
-#     cursor.execute(query)
-#     # grab the column headers from the returned data
-#     column_headers = [x[0] for x in cursor.description]
+# get the prices of menu items from a specific menu
+@vendors.route('/vendors/prices/<vendor_id>/<menu_id>',  methods=['GET'])
+def get_menu_prices(vendor_id, menu_id):
+    cursor = db.get_db().cursor()
+    query = 'SELECT mi.Name, mi.Price, mi.Ingredients, mi.Availability, mi.Description, mi.AllergenInformation FROM Vendor v JOIN Menu m on v.VendorID = m.VendorID'
+    query = query + ' JOIN MenuItems mi on mi.MenuID = m.MenuID'
+    query = query + ' WHERE (v.VendorID = ' + str(vendor_id) + ' and m.MenuID = ' + str(menu_id)
+    query = query + ') ORDER BY mi.Price DESC;'
 
-#     # create an empty dictionary object to use in 
-#     # putting column headers together with data
-#     json_data = []
+    cursor.execute(query)
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
 
-#     # fetch all the data from the cursor
-#     theData = cursor.fetchall()
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
 
-#     # for each of the rows, zip the data elements together with
-#     # the column headers. 
-#     for row in theData:
-#         json_data.append(dict(zip(column_headers, row)))
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
 
-#     return jsonify(json_data)
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
 
-# # get the list menu items from a specific menu
-# @vendors.route('/vendors/<vendor_id>/<menu_id>')
-# def get_menu_from_vendor(vendor_id, menu_id):
-#     cursor = db.get_db().cursor()
-#     query = '''
-#         SELECT mi.Name, mi.Price
-#         FROM Vendor v
-#         JOIN Menu m on v.VendorID = m.VendorID
-#         JOIN MenuItems mi on mi.MenuID = m.MenuID
-#         WHERE v.VendorID = ' + str(vendor_id) and m.MenuID = ' + str(menu_id)
-#     '''
-#     cursor.execute(query)
-#     # grab the column headers from the returned data
-#     column_headers = [x[0] for x in cursor.description]
+    return jsonify(json_data)
 
-#     # create an empty dictionary object to use in 
-#     # putting column headers together with data
-#     json_data = []
+# get the list menu items from a specific menu
+@vendors.route('/vendors/menu/', methods=['GET'])
+def get_menu_from_vendor():
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT v.VendorID, v.VendorName, v.VendorType, v.CuisineType, mi.MenuID
+        FROM Vendor v
+        JOIN Menu m on v.VendorID = m.VendorID
+        JOIN MenuItems mi on mi.MenuID = m.MenuID
+       ;'''
 
-#     # fetch all the data from the cursor
-#     theData = cursor.fetchall()
+    cursor.execute(query)
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
 
-#     # for each of the rows, zip the data elements together with
-#     # the column headers. 
-#     for row in theData:
-#         json_data.append(dict(zip(column_headers, row)))
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
 
-#     return jsonify(json_data)
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
 
-# # # get the top 5 products from the database
-# # @products.route('/mostExpensive')
-# # def get_most_pop_products():
-# #     cursor = db.get_db().cursor()
-# #     query = '''
-# #         SELECT product_code, product_name, list_price, reorder_level
-# #         FROM products
-# #         ORDER BY list_price DESC
-# #         LIMIT 5
-# #     '''
-# #     cursor.execute(query)
-# #     # grab the column headers from the returned data
-# #     column_headers = [x[0] for x in cursor.description]
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
 
-# #     # create an empty dictionary object to use in 
-# #     # putting column headers together with data
-# #     json_data = []
+    return jsonify(json_data)
 
-# #     # fetch all the data from the cursor
-# #     theData = cursor.fetchall()
-
-# #     # for each of the rows, zip the data elements together with
-# #     # the column headers. 
-# #     for row in theData:
-# #         json_data.append(dict(zip(column_headers, row)))
-
-# #     return jsonify(json_data)
-
-
-# # @products.route('/tenMostExpensive', methods=['GET'])
-# # def get_10_most_expensive_products():
+@vendors.route('/vendor', methods=['POST'])
+def add_new_vendor():
     
-# #     query = '''
-# #         SELECT product_code, product_name, list_price, reorder_level
-# #         FROM products
-# #         ORDER BY list_price DESC
-# #         LIMIT 10
-# #     '''
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
 
-# #     cursor = db.get_db().cursor()
-# #     cursor.execute(query)
+    #extracting the variable
+    name = the_data['VendorName']
+    vendor_type = the_data['VendorType']
+    cuisine_type = the_data['CuisineType']
+    phone = the_data['Phone']
+    email = the_data['Email']
+    eID = the_data['EmployeeID']
 
-# #     column_headers = [x[0] for x in cursor.description]
+    # Constructing the query
+    """ query = 'insert into Vendor (VendorName, VendorType, CuisineType, Phone, Email) values ("'
+    query += name + '", "'
+    query += vendor_type + '", "'
+    query += cuisine_type + '", "'
+    query += phone + '", "'
+    query += email + '"); ' """
 
-# #     # create an empty dictionary object to use in 
-# #     # putting column headers together with data
-# #     json_data = []
+    query = 'insert into Vendor (VendorName, VendorType, CuisineType, Phone, Email, EmployeeID) values("{}", "{}", "{}", "{}", "{}", {});'.format(name, vendor_type, cuisine_type, phone, email, eID)
 
-# #     # fetch all the data from the cursor
-# #     theData = cursor.fetchall()
+    current_app.logger.info(query)
 
-# #     # for each of the rows, zip the data elements together with
-# #     # the column headers. 
-# #     for row in theData:
-# #         json_data.append(dict(zip(column_headers, row)))
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
     
-# #     return jsonify(json_data)
-
-# @vendors.route('/vendor', methods=['POST'])
-# def add_new_vendor():
-    
-#     # collecting data from the request object 
-#     the_data = request.json
-#     current_app.logger.info(the_data)
-
-#     #extracting the variable
-#     name = the_data['VendorName']
-#     vendor_type = the_data['VendorType']
-#     cuisine_type = the_data['CuisineType']
-#     phone = the_data['Phone']
-#     email = the_data['Email']
-#     employee_id = the_data['EmployeeID']
-
-#     # Constructing the query
-#     query = 'insert into Vendor (VendorName, VendorType, CuisineType, Phone, Email, EmployeeID) values ("'
-#     query += name + '", "'
-#     query += vendor_type + '", "'
-#     query += cuisine_type + '", '
-#     query += phone + '", '
-#     query += email + '", '
-#     query += employee_id + '", '
-#     current_app.logger.info(query)
-
-#     # executing and committing the insert statement 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     db.get_db().commit()
-    
-#     return 'Success!'
-
-# # @products.route('/product', methods=['POST'])
-# # def add_new_product():
-    
-# #     # collecting data from the request object 
-# #     the_data = request.json
-# #     current_app.logger.info(the_data)
-
-# #     #extracting the variable
-# #     name = the_data['product_name']
-# #     description = the_data['product_description']
-# #     price = the_data['product_price']
-# #     category = the_data['product_category']
-
-# #     # Constructing the query
-# #     query = 'insert into products (product_name, description, category, list_price) values ("'
-# #     query += name + '", "'
-# #     query += description + '", "'
-# #     query += category + '", '
-# #     query += str(price) + ')'
-# #     current_app.logger.info(query)
-
-# #     # executing and committing the insert statement 
-# #     cursor = db.get_db().cursor()
-# #     cursor.execute(query)
-# #     db.get_db().commit()
-    
-# #     return 'Success!'
-
-# # ### Get all product categories
-# # @products.route('/categories', methods = ['GET'])
-# # def get_all_categories():
-# #     query = '''
-# #         SELECT DISTINCT category AS label, category as value
-# #         FROM products
-# #         WHERE category IS NOT NULL
-# #         ORDER BY category
-# #     '''
-
-# #     cursor = db.get_db().cursor()
-# #     cursor.execute(query)
-
-# #     json_data = []
-# #     # fetch all the column headers and then all the data from the cursor
-# #     column_headers = [x[0] for x in cursor.description]
-# #     theData = cursor.fetchall()
-# #     # zip headers and data together into dictionary and then append to json data dict.
-# #     for row in theData:
-# #         json_data.append(dict(zip(column_headers, row)))
-    
-# #     return jsonify(json_data)
+    return 'Success!'
 
 @vendors.route('/vendors/<vendor_id>', methods=['PUT'])
-def update_vendor(VendorID):
+def update_vendor(vendor_id):
     
     # collecting data from the request object 
     the_data = request.json
@@ -304,7 +192,7 @@ def update_vendor(VendorID):
     query += 'CuisineType = " ' + cuisine_type + '", '
     query += 'Phone = " ' + phone + '", '
     query += 'EmployeeID = " ' + employee_id + '", '
-    query += 'Email = "' + email + '" WHERE VendorID = ' + str(VendorID) + ';' 
+    query += 'Email = "' + email + '" WHERE VendorID = ' + str(vendor_id) + ';' 
     current_app.logger.info(query)
 
     # executing and committing the insert statement 
@@ -315,8 +203,8 @@ def update_vendor(VendorID):
     return 'Success!'
 
 @vendors.route('/vendors/<vendor_id>', methods=['DELETE'])
-def delete_vendor(VendorID):
+def delete_vendor(vendor_id):
     cursor = db.get_db().cursor()
-    cursor.execute('DELETE FROM Vendor WHERE VendorID = ' + str(VendorID) + ';')
+    cursor.execute('DELETE FROM Vendor WHERE VendorID = ' + str(vendor_id) + ';')
     db.get_db().commit()
     return 'Success!'
