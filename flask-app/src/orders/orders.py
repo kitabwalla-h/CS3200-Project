b5f2c1676e9c9003717c06589d349b9b71e720b9
kitabwalla-h <76460205+kitabwalla-h@users.noreply.github.com>
2023-12-05 17:29:01 -0500

    Update orders.py

--- flask-app/src/orders/orders.py
+++ flask-app/src/orders/orders.py
@@ -123,4 +123,25 @@ def delete_order(OrderID):
     db.get_db().commit()
     return 'Success!'
 
+ #add new order
+@orders.route('/orders/menuitems', methods = ['POST'])
+def add_new_order_menuitesm():
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    orderdetailsid = the_data['OrderDetailsID']
+    menuitemname = the_data['MenuItemName']
+
+    # Constructing the query
+    query = 'insert into MenuItemsInOrder (OrderDetailsID, MenuItemName) values ("{}", "{}");'.format(orderdetailsid, menuitemname)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
 
