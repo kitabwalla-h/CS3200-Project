47bc85857f76d2e77bd923560fd0b7153b98a47b
ebalo03 <148168183+ebalo03@users.noreply.github.com>
2023-12-05 19:30:18 -0500

    Update customers.py

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -71,18 +71,18 @@ def update_customer(CustomerID):
     current_app.logger.info(the_data)
 
     #extracting the variable
-    first_name = the_data['first_name']
-    last_name = the_data['last_name']
-    phone = the_data['phone']
-    email = the_data['email']
+    FirstName = the_data['FirstName']
+    LastName = the_data['LastName']
+    Phone = the_data['Phone']
+    Email = the_data['Email']
     
 
     # Constructing the query
     query = 'update Customer SET '
-    query += 'FirstName = "' + first_name + '", '
-    query += 'LastName = "' + last_name + '", '
-    query += 'Phone = " ' + phone + '", '
-    query += 'Email = "' + email + '" WHERE CustomerID = ' + str(CustomerID) + ';' 
+    query += 'FirstName = "' + FirstName + '", '
+    query += 'LastName = "' + LastName + '", '
+    query += 'Phone = " ' + Phone + '", '
+    query += 'Email = "' + Email + '" WHERE CustomerID = ' + str(CustomerID) + ';' 
     current_app.logger.info(query)
 
     # executing and committing the insert statement 
@@ -97,4 +97,48 @@ def delete_customer(CustomerID):
     cursor = db.get_db().cursor()
     cursor.execute('DELETE FROM Customer WHERE CustomerID = ' + str(CustomerID) + ';')
     db.get_db().commit()
+
+
+# getting customer location
+@customers.route('/customers/location', methods=['GET'])
+def get_customer_location():
+    cursor = db.get_db().cursor()
+    cursor.execute('Select * from CustomerLocations;')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# updating customer location
+@customers.route('/customers/location/<CustomerID>', methods=['POST'])
+def update_customer_location(CustomerID):
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    Street = the_data['Street']
+    City = the_data['City']
+    State = the_data['State']
+    ZipCode = the_data['ZipCode']
+
+    # Constructing the query
+    query = 'update CustomerLocations SET '
+    query += 'Street = "' + Street + '", '
+    query += 'City = "' + City + '", '
+    query += 'State = " ' + State + '", '
+    query += 'ZipCode = "' + ZipCode + '" WHERE CustomerID = ' + str(CustomerID) + ';' 
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
     return 'Success!'
